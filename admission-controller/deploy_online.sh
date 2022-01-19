6c03cba5b904c1d8cc0ed4b277e23caa80f0772e
hyunjunpyo <hyunjun8982@t3q.com>
2022-01-19 16:06:32 +0900

    edit deploy.sh

--- admission-controller/deploy_online.sh
+++ admission-controller/deploy_online.sh
@@ -3,7 +3,7 @@ IMAGE = admission-controller-webhook-daemon:latest
 NAMESPACE = mutate-server-test
 
 # 1. Build docker image
-docker build -t $IMAGE image/
+docker build -t $IMAGE -f image/Dockerfile .
 
 # 2. Create namespace for demo
 kubectl create namespace $NAMESPACE
@@ -14,7 +14,8 @@ openssl genrsa -out ca.key 2048
 openssl req -new -x509 -days 365 -key ca.key -subj "/C=CN/ST=GD/L=SZ/O=Acme, Inc./CN=Acme Root CA" -out ca.crt
 
 openssl req -newkey rsa:2048 -nodes -keyout server.key -subj "/C=CN/ST=GD/L=SZ/O=Acme, Inc./CN=mutate-server-svc.$NAMESPACE.svc" -out server.csr
-openssl x509 -req -extfile <(printf "subjectAltName=DNS:mutate-server-svc.$NAMESPACE.svc") -days 365 -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt
+printf "subjectAltName=DNS:mutate-server-svc.$NAMESPACE.svc" > tmp-ext-file
+openssl x509 -req -extfile tmp-ext-file -days 365 -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt
 
 # 3.2. Create TLS secret for service
 kubectl create secret tls webhook-certs --cert "server.crt" --key "server.key" -n $NAMESPACE
@@ -24,7 +25,7 @@ cat yaml/webhook-mutate-deploy.yaml | sed "s/{{NAMESPACE}}/$NAMESPACE/g" | kubec
 
 # 5. Register Mutator Server (Flask) as a Mutate Webhook to Kubernetes
 export CA_PEM_BASE64="$(openssl base64 -A <"ca.crt")"
-cat yaml/mutate-webhook-conf.yaml | sed "s/{{CA_PEM_BASE64}}/$CA_PEM_BASE64/g" | kubectl apply -n $NAMESPACE -f -
+cat yaml/mutate-webhook-conf.yaml | sed "s/{{CA_PEM_BASE64}}/$CA_PEM_BASE64/g" | sed "s/{{NAMESPACE}}/$NAMESPACE/g" | kubectl apply -n $NAMESPACE -f -
 
 # 6. Clean secret files
-rm -rf webhook-server-tls.* ca.*
+rm -rf server.* ca.* tmp-ext-file
